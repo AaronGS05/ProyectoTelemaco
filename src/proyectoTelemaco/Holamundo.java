052761e512e28361d71b22c9c6d0bb174f7527bf
aarongs <aguestas02@educarex.es>
2023-12-01 12:05:24 +0100

    Version1

--- src/proyectoTelemaco/Holamundo.java
+++ src/proyectoTelemaco/Holamundo.java
@@ -5,6 +5,6 @@ public class Holamundo {
 	{
 		System.out.println("Hola Mundo");
 		System.out.println("Buenos dias");
-		System.out.println("Estoy en la RAMA1");
+		System.out.println("Estoy en la RAMA.1");
 	}
 }
